671e4e0145b3f1dc5d4d4c868f841ea966ea78f2
jordanforbes <jordforb@gmail.com>
2020-09-07 21:46:52 -0400

    created header component

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+
+import Header from './components/Header'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return ( 
+        <View style = {{backgroundColor: 'black', padding:100, flex: 1,}} >
+            <Header title="CRONOSO"/>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: 'gray',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    title:{
+      color:'white'
+    }
 });
